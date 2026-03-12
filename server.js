3cde215455f9f79691bac97b0604ef700d9568ae
Prerna Sharma <prernasharma181103@gmail.com>
2026-03-12 20:10:33 +0530

    fix render port

--- server.js
+++ server.js
@@ -33,4 +33,8 @@ app.post("/generate", async (req, res) => {
     res.json({ quiz, time });
 });
 
-app.listen(3000, () => console.log("Server running on 3000"));
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+   console.log("Server running on port " + PORT)
+})
